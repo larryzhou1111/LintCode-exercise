8e8c3a77868e181dfc84e88f0793ad69cc6b4447
larry <larryzhou1111@163.com>
2017-06-04 23:49:30 +0800

    2017.6.4

---
+++ 14_first-position-of-target/first-position-of-target.java
@@ -0,0 +1,43 @@
+/*
+@Copyright:LintCode
+@Author:   larryzhou
+@Problem:  http://www.lintcode.com/problem/first-position-of-target
+@Language: Java
+@Datetime: 17-06-04 15:34
+*/
+
+class Solution {
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+        //write your code here
+        
+        //二分查找,第一个下标。区别于题457 ！
+        int low = 0;
+        int high = nums.length - 1;
+        int mid;
+        
+        while(low < high){
+            
+            //防止low + high 溢出
+            mid = (high - low) / 2 + low;
+            
+            if(target < nums[mid]){
+                high = mid - 1;
+            }
+            else if(target > nums[mid]){
+                low = mid + 1;
+            }
+            else
+                high = mid;
+        }
+        
+        if(target == nums[high])
+            return high;
+            
+        return -1;
+    }
+}
