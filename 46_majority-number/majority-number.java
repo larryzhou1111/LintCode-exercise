7af0c83ba3b5c441b17c054f6a7eedca2a22e17e
larry <larryzhou1111@163.com>
2017-06-11 23:46:34 +0800

    2017.6.11

---
+++ 46_majority-number/majority-number.java
@@ -0,0 +1,51 @@
+/*
+@Copyright:LintCode
+@Author:   larryzhou
+@Problem:  http://www.lintcode.com/problem/majority-number
+@Language: Java
+@Datetime: 17-06-11 14:55
+*/
+
+public class Solution {
+    /**
+     * @param nums: a list of integers
+     * @return: find a  majority number
+     */
+    public int majorityNumber(ArrayList<Integer> nums) {
+        // write your code
+        
+        //方法1：分治法
+        /*
+          保持两个标记变量。一个统计值count初始为0，一个当前值cur
+              遍历整个数组，用x表示当前值
+          当count == 0时，设cur = x, count++
+          当count != 0时，若x == cur,则count++; 若x != cur,则count--
+          最后返回cur
+        */
+        
+        int count = 0;
+        int cur = nums.get(0);
+        
+        for(int i = 0; i < nums.size(); i++){
+            
+            //count减到0时，说明当前保存的元素，出现的次数等于数组元素个数的二分之一
+            //则舍弃当前元素cur，寻找下一个元素作为cur
+            if(count == 0){
+                
+                cur = nums.get(i);
+                count = 1;
+            }
+            
+            else{
+                
+                if(cur == nums.get(i)){
+                    count++;
+                }
+                else{
+                    count--;
+                }
+            }
+        }
+        return cur;        
+    }
+}
