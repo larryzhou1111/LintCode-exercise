fb7eb62db0188dac5b8edba102d0b823f516ffcb
larry <larryzhou1111@163.com>
2017-06-22 23:23:46 +0800

    2017.6.22

---
+++ 6-merge-two-sorted-arrays/merge-two-sorted-arrays.java
@@ -0,0 +1,60 @@
+/*
+@Copyright:LintCode
+@Author:   larryzhou
+@Problem:  http://www.lintcode.com/zh-cn/problem/merge-two-sorted-arrays/
+@Language: Java
+@Datetime: 17-06-22 20:54
+*/
+
+class Solution {
+    /**
+     * @param A and B: sorted integer array A and B.
+     * @return: A new sorted integer array
+     */
+    public int[] mergeSortedArray(int[] A, int[] B) {
+        // Write your code here
+        
+        //思路：新建数组C，长度为A和B长度之和
+        //从后往前遍历A和B，大的元素添加到数组C尾部。A和B长度不重叠部分，直接归并即可
+        
+        if(A == null)
+            return B;
+        
+        if(B == null)
+            return A; 
+            
+        int i = A.length - 1;
+        int j = B.length - 1;
+        
+        //新数组的最后一个元素位置    
+        int len = A.length + B.length - 1;
+        int[] C = new int[A.length + B.length];
+        
+        //从后往前数，数组A和B重合的部分
+        while(i >= 0 && j >= 0){
+            
+            if(A[i] > B[j]){
+                
+                C[len--] = A[i--];
+            }
+            else{
+                
+                C[len--] = B[j--];
+            }
+        }
+
+        //数组A的长度更长时
+        while (i >= 0){  
+            
+            C[len--] = A[i--];
+        }  
+        
+        //数组B的长度更长时
+        while (j >= 0){  
+            
+            C[len--] = B[j--]; 
+        }
+        
+        return C;
+    }
+}
