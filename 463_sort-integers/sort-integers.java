7face60eab1477de1616b6107a6974eb7066a01e
larry <larryzhou1111@163.com>
2017-06-23 23:26:20 +0800

    2017.6.23

--- 463_sort-integers/sort-integers.java
+++ 463_sort-integers/sort-integers.java
@@ -13,6 +13,9 @@ public class Solution {
      */
     public void sortIntegers(int[] A) {
         // Write your code here
+        
+        //类比题464
+        
     /*    
         //冒泡排序
         int i,j;
@@ -88,34 +91,36 @@ public class Solution {
                 
             }
         }
-      } 
+    } 
       */
   
-      //插入排序
-      /*
+        //插入排序
+        /*
          * 从第一个元素开始，该元素可以认为已经被排序
          * 取出下一个元素，在已经排序的元素序列中从后向前扫描 
          * 如果该元素（已排序）大于新元素，将该元素移到下一位置  
          * 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置  
          * 将新元素插入到该位置中  
          * 重复步骤2 
-      */
-      int i, j, k;
+        */
+        int i, j, k;
       
-      for(i = 0; i < A.length; i++){
+        for(i = 0; i < A.length; i++){
           
-          k = A[i];
+            k = A[i];
           
-          //假如k比前面的值小，则将前面的值后移
-          for(j = i; j > 0 && k < A[j - 1]; j--){
+            //假如k比前面的值小，则将前面的值后移
+            for(j = i; j > 0 && k < A[j - 1]; j--){
               
-              A[j] = A[j - 1];
-          }
-          A[j] = k;
-          
+                A[j] = A[j - 1];
+            }
+            
+            A[j] = k;
+            
+        }
     }
-  }
 }
+
 
 
 
