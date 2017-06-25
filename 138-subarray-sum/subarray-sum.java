507c2f4d9618db3ad1c6c7ba435d510e54d19449
larry <larryzhou1111@163.com>
2017-06-25 12:19:41 +0800

    2017.6.25

--- 138-subarray-sum/subarray-sum.java
+++ 138-subarray-sum/subarray-sum.java
@@ -16,7 +16,7 @@ public class Solution {
         // write your code here
         
         //思路：使用Map来记录index, sum的值
-        //当遇到两个index的sum相同时，表示从index1+1到index2是一个解
+        //当遇到两个index的sum相同时，表示从 index1+1 到 index2 也是一个解
         //注意：添加一个index = -1作为虚拟节点。这样我们才可以记录index1 = 0的解
        
         ArrayList<Integer> result = new ArrayList<Integer>();
