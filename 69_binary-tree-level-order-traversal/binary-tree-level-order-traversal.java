3f8c994873ad044820f3500ef48a7fe8a03c8c23
larry <larryzhou1111@163.com>
2017-07-03 14:11:42 +0800

    2017.7.3

--- 69_binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 69_binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -19,6 +19,19 @@
  */
  
  
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+ 
+ 
 public class Solution {
     /**
      * @param root: The root of binary tree.
@@ -26,35 +39,46 @@ public class Solution {
      */
     public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
         // write your code here
+		
+		/* 二叉树的层次遍历
+		
+		   给出一棵二叉树，返回其节点值的层次遍历（逐层从左往右访问）
+		   
+		   二叉树 {3,9,20,#,#,15,7} ：返回他的分层遍历结果：
+				[
+				  [3],
+				  [9,20],
+				  [15,7]
+				]
+		*/
         
         ArrayList result = new ArrayList();
  
-        if (root == null) {
+        if(root == null)
             return result;
-        }
- 
+
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.offer(root);
  
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             
             ArrayList<Integer> level = new ArrayList<Integer>();
             
             int size = queue.size();
             
-            for (int i = 0; i < size; i++) {
+            for(int i = 0; i < size; i++){
                 
                 TreeNode head = queue.poll();
-                // add value to result level
+                
                 level.add(head.val);
                 
-                if (head.left != null) {
-                    // add left to queue
+                if(head.left != null){
+                    
                     queue.offer(head.left);     
                 }
                 
-                if (head.right != null) {
-                    // add right to queue
+                if(head.right != null){
+                    
                     queue.offer(head.right);    
                 }
             }
