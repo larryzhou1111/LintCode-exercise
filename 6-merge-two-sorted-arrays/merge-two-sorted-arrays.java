56bcbed64babe0eb73a7d5e0002761dfa454c510
larry <larryzhou1111@163.com>
2017-07-16 22:00:25 +0800

    2017.7.16

--- 6-merge-two-sorted-arrays/merge-two-sorted-arrays.java
+++ 6-merge-two-sorted-arrays/merge-two-sorted-arrays.java
@@ -14,6 +14,13 @@ class Solution {
     public int[] mergeSortedArray(int[] A, int[] B) {
         // Write your code here
         
+        /* 合并排序数组:合并两个排序的整数数组A和B变成一个新的数组
+            样例:
+            给出A=[1,2,3,4]，B=[2,4,5,6]，返回 [1,2,2,3,4,4,5,6]
+        */
+        
+        //类比题64，题212（剑指offer题4）
+        
         //思路：新建数组C，长度为A和B长度之和
         //从后往前遍历A和B，大的元素添加到数组C尾部。A和B长度不重叠部分，直接归并即可
         
@@ -44,13 +51,13 @@ class Solution {
         }
 
         //数组A的长度更长时
-        while (i >= 0){  
+        while(i >= 0){  
             
             C[len--] = A[i--];
         }  
         
         //数组B的长度更长时
-        while (j >= 0){  
+        while(j >= 0){  
             
             C[len--] = B[j--]; 
         }
