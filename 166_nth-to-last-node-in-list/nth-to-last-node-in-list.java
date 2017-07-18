0953d31f5b32ae7a8f539404e21385c64d41ca74
larry <larryzhou1111@163.com>
2017-07-18 23:39:36 +0800

    2017.7.18

--- 166_nth-to-last-node-in-list/nth-to-last-node-in-list.java
+++ 166_nth-to-last-node-in-list/nth-to-last-node-in-list.java
@@ -28,11 +28,11 @@ public class Solution {
         
         //需回顾！
         
-        //方法1：递归法
+        //方法1：递归法，实际遍历了链表两次，效率低
+        /*
         //当递归到链表尾部时返回，每次返回时长度加1，一旦长度为n时记录下该节点
-        if(head == null || n < 0) {
+        if(head == null || n <= 0) 
             return null;
-        }
         
         int count = 0;
         ListNode node = head;
@@ -48,22 +48,43 @@ public class Solution {
             head = head.next;
         }
         return head;
-    
+        */
  
-        //方法2：用两个指针，快指针先走N步，然后快慢指针同时开始走，保持N的距离
-        //       这样当快指针到达末尾时，慢指针就是倒数第N个
-        /*
-        ListNode slow = head;
+        //方法2：
+        /*  
+            用两个指针，快指针先走n-1步，
+            然后快慢指针同时开始走，保持n-1的距离
+            这样当快指针到达末尾时，慢指针就是倒数第n个
+        */
+        if(head == null || n <= 0) 
+            return null;
+        
+        ListNode slow = null;
         ListNode fast = head;
         
-        while(n-- > 0){
-            fast = fast.next;
-        }
-        while(fast != null){
+        //快指针先走n-1步
+        for(int i = 0; i < n - 1; i++){
+            
+            if(fast.next != null){
+                
+                fast = fast.next;
+            }
+            
+            //考虑链表实际长度，小于n的情况
+            else
+                return null;
+                
+        }  
+  
+        slow = head;
+        
+        while(fast.next != null){
+            
             fast = fast.next;
             slow = slow.next;
         }
+        
         return slow;
-        */
     }
 }
+
