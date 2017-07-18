8d3600c1a5a2a096b5579d53c5acdebeeee2542f
larry <larryzhou1111@163.com>
2017-07-18 23:38:35 +0800

    2017.7.18

--- 35-reverse-linked-list/reverse-linked-list.java
+++ 35-reverse-linked-list/reverse-linked-list.java
@@ -25,6 +25,13 @@ public class Solution {
     public ListNode reverse(ListNode head) {
         // write your code here
         
+        /* 翻转链表
+        
+          样例:
+          给出一个链表1->2->3->null，这个翻转后的链表为3->2->1->null
+        */
+
+        //方法1：
         /*
           翻转链表，将链表从最后一个节点指向前面的节点。 
           遍历链表，先完成一个操作 1->2 变成 2->1 
@@ -33,21 +40,35 @@ public class Solution {
           如果直接把2->的元素变为1，那原本2->的元素将丢失。 
           故需要先保存2->的节点再进行此操作。 
           另外还需要一个元素记录前一个节点，即是每循环一步生成的新链表的头部
-        */
-        if(head == null)
-            return null;
-            
-        ListNode p = head;
-        ListNode next = null;
-        ListNode pre = null;
+        */  
         
-        while(p != null){
+        /*
+        if(head == null || head.next == null)
+            return head;
             
-            next = p.next;
-            p.next = pre;
-            pre = p;
-            p = next;
+        ListNode pre = null;            
+        ListNode cur = head;
+        ListNode next = null;
+        
+        while(cur != null){
+            
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
         }
+        
         return pre;
+        */
+        
+        //方法2：递归实现
+        if(head == null || head.next == null)
+            return head;        
+        
+        ListNode node = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        
+        return node; 
     }
 }
