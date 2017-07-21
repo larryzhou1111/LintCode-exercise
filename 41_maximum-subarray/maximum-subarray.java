60337f19782c1ff8b327e0edd7aeccca93dfc5df
larry <larryzhou1111@163.com>
2017-07-21 23:13:26 +0800

    2017.7.21

--- 41_maximum-subarray/maximum-subarray.java
+++ 41_maximum-subarray/maximum-subarray.java
@@ -14,36 +14,46 @@ public class Solution {
     public int maxSubArray(int[] nums) {
         // write your code
         
-        //最大子数组，类比题44, 但本题是整型数组，注意区别！
+		/* 最大子数组 :给定一个整数数组，找到一个具有最大和的子数组，返回其最大和。
+
+			注意事项:子数组最少包含一个数
+
+			样例: 给出数组[−2,2,−3,4,−1,2,1,−5,3]，符合要求的子数组为[4,−1,2,1]，其最大和为6		
+		*/
+		
+        //最大子数组，类比题44, 但本题是整型数组，注意区别！（剑指offer题31）
         
         //方法1：暴力枚举。效率低，略
         
-        
         //方法2：贪心法，复杂度O（n）
-        //将子串和为负数的子串丢掉，只留和为正的子串
+        //将子串和为负数的子串丢掉，更新为当前的数组元素；若子串和为正，继续累加
         /*
+        if(nums == null || nums.length == 0)
+            return Integer.MIN_VALUE;
+            
         int max = Integer.MIN_VALUE;  
         int sum = 0;  
         
         for(int i = 0; i < nums.length; i++){  
-            
-            sum += nums[i];  
+
+            if(sum < 0){  
+                
+                //子串和为负数，丢掉，更新为当前的数组元素
+                sum = nums[i];     
+            }
+            else{
+                sum += nums[i];
+            }
             
             if(sum > max){  
                 
                 max = sum;  
             } 
-            
-            if(sum < 0){  
-                
-                //子串和为负数，丢掉
-                sum = 0;     
-            }  
+
         }  
         
         return max;         
         */
-        
         
         //方法3：动态规划法
         /*
@@ -54,13 +64,16 @@ public class Solution {
           给出数组[−2,2,−3,4,−1,2,1,−5,3]，符合要求的子数组为[4,−1,2,1]，其最大和为6
           
           nums	            -2	 2	-3	 4	-1	 2	 1	-5	 3
-          
+                               /
           max_ending_here	-2	 2	-1	 4	 3	 5	 6	 1	 4
-         
+                               /
           max_so_far	    -2	 2	 2	 4	 4	 5	 6	 6	 6
           
         */
         
+        if(nums == null || nums.length == 0)
+            return Integer.MIN_VALUE;
+            
         int max_ending_here = nums[0];
         int max_so_far = nums[0];
         
@@ -75,6 +88,5 @@ public class Solution {
         }
         
         return max_so_far; 
-        
     }
 }
