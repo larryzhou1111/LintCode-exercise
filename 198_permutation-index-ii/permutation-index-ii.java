503d08651ddaed246af5b5de55df1cdb81419861
larry <larryzhou1111@163.com>
2017-07-21 23:13:13 +0800

    2017.7.21

--- 198_permutation-index-ii/permutation-index-ii.java
+++ 198_permutation-index-ii/permutation-index-ii.java
@@ -14,10 +14,20 @@ public class Solution {
     public long permutationIndexII(int[] A) {
         // Write your code here
         
-        //类比题197，区别在于本题含有重复数字，需要排除重复排列
-        //如果A[i]在[0,i]区间中有重复元素
-        //则用A[i]的 count * fact 的结果,除以重复次数dup的阶乘
+        /*  排列序号II
         
+            给出一个可能包含重复数字的排列，
+            求这些数字的所有排列按字典序排序后，该排列在其中的编号。编号从1开始
+            
+            样例
+            给出排列[1, 4, 2, 2]，其编号为3
+        */
+        
+        //类比题197，区别在于本题含有重复数字，需要排除重复排列
+        /*
+            如果A[i]在[0,i]区间中有重复元素
+            则用A[i]的 count * fact 的结果,除以重复次数dup的阶乘
+        */
         if(A == null || A.length == 0) 
             return 0;
             
@@ -36,8 +46,11 @@ public class Solution {
                 map.put(A[i], 1);
             }
             else{
+                
                 //把A[i]对应的value值，替换为重复次数
                 map.put(A[i], map.get(A[i]) + 1);
+                
+                //累乘，得到重复次数dup的阶乘
                 dup *= map.get(A[i]);
             }            
             
@@ -49,22 +62,12 @@ public class Solution {
                     
                     rank++;
                 }   
-                /*
-                下述循环写法是有问题的！
-                for(int k = j + 1; k < A.length; k++){  
-                
-                    if(A[j] == A[k] && A[i] > A[k]){
-                        
-                        dup++;
-                    }
-                }
-                */
             }
             
             index += rank * factor / dup;
             factor *= (A.length - i);
         }
-        return index;        
         
+        return index;        
     }
 }
