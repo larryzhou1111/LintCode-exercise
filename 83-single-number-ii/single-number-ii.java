099bb656c946a9c47559b226bb8ad9d4f4d2e863
larry <larryzhou1111@163.com>
2017-07-22 23:13:33 +0800

    2017.7.22

--- 83-single-number-ii/single-number-ii.java
+++ 83-single-number-ii/single-number-ii.java
@@ -14,16 +14,23 @@ public class Solution {
     public int singleNumberII(int[] A) {
         // write your code here
         
-        //需要回顾！！！
+        /* 落单的数 II
         
-        /*方法1：
-              思路：按位计算
-              int型数字占32位，如果这个数字出现3次
-              则与这个数字对应的每一位上的1也出现三次
-              使用int型数组记录每一位上1出现的次数，能被3整除,则表示出现3次
-              最后得到的就是要求的数字
+            给出3*n + 1 个的数字，除其中一个数字之外其他每个数字均出现三次，
+            找到这个数字
+        
+            样例:给出 [1,1,2,3,3,3,2,2,4,1] ，返回 4
         */
-        /*
+        
+        /*方法1：思路：按位计算
+        
+          int型数字占32位，如果这个数字出现3次，
+          则与这个数字对应的每一位上的1也出现三次
+          
+          使用int型数组记录每一位上1出现的次数，能被3整除,则表示出现3次
+          最后得到的就是要求的数字
+        */
+        
         if(A == null || A.length == 0)
             return 0;
             
@@ -34,14 +41,29 @@ public class Solution {
             
             for(int j = 0; j < A.length; j++){
                 
+                // >>=1 等价于 /2，且位运算效率更高
+                
+                //此处判断第i位，1出现的次数
                 bits[i] += A[j]>>i & 1;
             }
             
             bits[i] = bits[i] % 3;
+            /*
+                两种逻辑或（||和|）的运算规则基本相同
+                
+                |运算是把逻辑表达式全部计算完，而||运算具有短路计算功能
+                
+                对于 || 运算来说，只要运算符左端的值为true，
+                则无论运算符右端的值为true或为false，其最终结果都为true
+                所以一旦判断出||运算符左端的值为true，则系统将终止其后的计算过程
+                
+                此处只能用"|"，result 和 bits[i] << i 同为0时，result才为0
+            */
             result = result | bits[i] << i;
         }
+        
         return result;
-        */
+        
         
         //方法2：
         /*
@@ -49,7 +71,8 @@ public class Solution {
           当a出现三次的时候，ones和twos都清零 
           所以，如果一个数值中所有的数都通过这个循环的话，出现三次的数都清零了， 
           有一个数如果出现一次，它保存在ones中；如果出现两次的话保存在twos中。
-          */
+        */
+        /*
         if(A == null || A.length == 0)
             return 0;
             
@@ -58,10 +81,12 @@ public class Solution {
         
         for (int i = 0; i < A.length; i++){  
             
+            //非运算符"~": 如果位为0，结果是1；如果位为1，结果是0
             ones = (ones ^ A[i]) & (~twos);  
             twos = (twos ^ A[i]) & (~ones);  
         }  
   
         return ones; 
+        */
     }
 }
