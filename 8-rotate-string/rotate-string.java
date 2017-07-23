15fa25b97ad6d75853ed5b31e8d775aa1252b18b
larry <larryzhou1111@163.com>
2017-07-23 19:09:21 +0800

    2017.7.23

--- 8-rotate-string/rotate-string.java
+++ 8-rotate-string/rotate-string.java
@@ -15,36 +15,58 @@ public class Solution {
     public void rotateString(char[] str, int offset) {
         // write your code here
         
-        /*
-        //方法1：在原地旋转，旋转三次可以达到效果，需回顾！
-        if (str == null || str.length == 0)
+        /* 旋转字符串 （右旋）(类比剑指offer题42-2,左旋)
+        
+            给定一个字符串和一个偏移量，根据偏移量旋转字符串(从左向右旋转)
+            
+            样例：对于字符串 "abcdefg".
+
+                offset=0 => "abcdefg"
+                offset=1 => "gabcdef"
+                offset=2 => "fgabcde"
+                offset=3 => "efgabcd"
+        */
+        
+        //方法1：在偏移点处将字符串切割，然后倒序拼接。代码略
+        //str.substring(start, end)求子字符串，包括start，不包括end
+        
+        //方法2：原地旋转三次，可以达到效果 
+        if(str == null || str.length == 0 || offset < 0)
             return;
 
         offset = offset % str.length;
-        
-        //注意旋转三次各自的偏移量
+
+        //偏移点的前半部分，旋转
         reverse(str, 0, str.length - offset - 1);
+        
+        //偏移点的后半部分，旋转
         reverse(str, str.length - offset, str.length - 1);
+        
+        //整个字符串旋转
         reverse(str, 0, str.length - 1);
+        
     }
 
     private void reverse(char[] str, int start, int end){
         
-        for(int i = start, j = end; i < j; i++, j--){
+        while(start < end){
+  
             //交换
-            char temp = str[i];
-            str[i] = str[j];
-            str[j] = temp;
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            
+            start++;
+            end--;
         }
-        */
-        
-		//方法2：与上述方法类似
-        if(str == null || str.length == 0) 
+         
+        /*
+        if(str == null || str.length == 0 || offset < 0) 
             return;
             
         offset = offset % str.length;
         
-        for(int i  = 0; i < str.length / 2; i++){
+        for(int i = 0; i < str.length / 2; i++){
             
             //整个字符串翻转
             char c = str[i];
@@ -67,5 +89,6 @@ public class Solution {
             str[offset + i] = str[str.length - 1 - i];
             str[str.length - 1 - i] = c;
         }
+        */
     }
 }
