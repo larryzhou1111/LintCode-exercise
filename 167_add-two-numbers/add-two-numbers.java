f385d3574d331773f6872cff4d858caecf1aa134
larry <larryzhou1111@163.com>
2017-07-24 22:46:48 +0800

    2017.7.24

--- 167_add-two-numbers/add-two-numbers.java
+++ 167_add-two-numbers/add-two-numbers.java
@@ -26,10 +26,18 @@ public class Solution {
     public ListNode addLists(ListNode l1, ListNode l2) {
         // write your code here
         
-        if (l1 == null)  
+        /* 链表求和
+            你有两个用链表代表的整数，其中每个节点包含一个数字。
+            数字存储按照在原来整数中相反的顺序，使得第一个数字位于链表的开头。
+            写出一个函数将两个整数相加，用链表形式返回和
+            
+            样例:
+            给出两个链表 3->1->5->null 和 5->9->2->null，返回 8->0->8->null
+        */
+        if(l1 == null)  
             return l2; 
             
-        else if (l2 == null)  
+        else if(l2 == null)  
             return l1;  
         
         //定义进位和临时变量    
@@ -39,39 +47,48 @@ public class Solution {
         ListNode head = new ListNode(0);  
         ListNode p = head;
         
-        while (l1 != null && l2 != null)  
-        {  
+        while(l1 != null && l2 != null){  
+            
             temp = l1.val + l2.val + c;  
             c = temp / 10;  
             temp = temp % 10;  
+            
             p.next = new ListNode(temp);  
+            
             p = p.next;  
             l1 = l1.next;  
             l2 = l2.next;  
         }  
-        while (l1 != null)  
-        {  
+        
+        while(l1 != null){  
+            
             temp = l1.val + c;  
             c = temp / 10;  
             temp = temp % 10;  
+            
             p.next = new ListNode(temp);  
+            
             p = p.next;  
             l1 = l1.next;  
         }  
-        while (l2 != null)  
-        {  
+        
+        while(l2 != null){  
+            
             temp = l2.val + c;  
             c = temp / 10;  
             temp = temp % 10;  
+            
             p.next = new ListNode(temp);  
+            
             p = p.next;  
             l2 = l2.next;  
         }  
-        if (c != 0)  
-        {  
-            p.next = new ListNode(c);  
-        }  
-        return head.next;  
         
+        if(c != 0){  
+            
+            p.next = new ListNode(c);  
+        } 
+        
+        return head.next;  
     }
 }
--- 1_a-b-problem/a-b-problem.java
+++ 1_a-b-problem/a-b-problem.java
@@ -15,6 +15,16 @@ class Solution {
     public int aplusb(int a, int b) {
         // write your code here, try to do it without arithmetic operators.
         
+        /* A + B 问题: 给出两个整数a和b, 求他们的和, 但不能使用 + 等数学运算符
+        
+            注意事项:不需要从输入流读入数据，只需要根据aplusb的两个参数a和b，
+            计算他们的和并返回就行
+            
+            说明: a和b都是32位整数;可以使用位运算符
+
+            样例: 如果 a=1 并且 b=2，返回3
+        */
+        
         /*思路：
             进行二进制的位运算
               1、(位异或^) ：如果相同位不同则为1，相同位相同则为0
@@ -23,15 +33,32 @@ class Solution {
                         目的是将相加后需要进位的单独取出
               3、重复以上2步，直到其中一方全为0，输出结果即可。
         */
-        
-        int sum,plusnum;  
-        if(b==0)  
-            return a; 
+        if(b == 0)  
+            return a;  
+            
+        int sum, plusnum;  
+
         //相加不进位，如果两个位都是0或1，则相加为0，不相同时则为1   
-        sum = a^b;  
+        sum = a ^ b;  
+        
         //相加只进位，只有当两个位都是1时才进位，其它情况不进位，然后左移,最后一位进位为0 
-        plusnum = (a&b)<<1; 
+        plusnum = (a & b) << 1; 
+        
         //递归直到没有位可进
-        return aplusb(sum,plusnum); 
+        return aplusb(sum, plusnum); 
+		
+		/* 
+			类似题：不使用新的变量，交换两个变量a、b的值
+			
+		    (1)方法1，基于加减法：
+				a = a + b;
+				b = a - b;
+				a = a - b;
+			
+			(2)方法2，基于异或运算
+				a = a ^ b;
+				b = a ^ b;
+				a = a ^ b;
+		*/
     }
 }
